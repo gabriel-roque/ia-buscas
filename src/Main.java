6fd5bf28ceb57b2466f8fd2ad8379cef1f189bda
gabriel-roque <gabrielrms.dll@gmail.com>
2020-05-21 21:18:47 -0300

    feat: update import packages;

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import espacoDeEstados.*;
+import estrategiasDeBusca.cega.*;
 import estrategiasDeBusca.heuristica.*;
 
 public class Main {
@@ -6,22 +7,29 @@ public class Main {
 	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) {
 		
-		//char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
-		//char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
-		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
-		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+//		char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
+//		char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
+//		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
+		char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
 
 		Puzzle8 puzzleInicial = new Puzzle8();
 		puzzleInicial.setEstado(cfgIni);
 		puzzleInicial.setCusto(0);
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
-			
+
 		Puzzle8 puzzleFinal = new Puzzle8();
 		puzzleFinal.setEstado( Puzzle8.TABULEIRO_ORGANIZADO );
 		puzzleFinal.setCusto(0);
 		puzzleFinal.setAvaliacao(0);
 						
-		BuscaInformada busca = new AStar();
+//		BuscaCega busca = new BuscaEmLargura();
+//		BuscaCega busca = new BuscaEmProfundidade();
+//		BuscaCega busca = new BuscaEmProfundidadeLimitada(null, null, 16);
+
+//		BuscaInformada busca = new BestFirst();
+//		BuscaInformada busca = new AStar();
+		BuscaInformada busca = new HillClimbing();
+
 		busca.setInicio(puzzleInicial);
 		busca.setObjetivo(puzzleFinal);
 		busca.buscar();
